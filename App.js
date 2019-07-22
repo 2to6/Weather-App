53d5f6a72ddb2f0d61f5563729b51de5f0f6041f
Choejiwon <jiwon0929@ajou.ac.kr>
2019-07-22 20:50:03 +0900

    06. state: counter

--- App.js
+++ App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { Button ,StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component{
-  clickHandler = () => console.log('Clicked!');
+  state = {
+    counter: 0,
+  };
+
+  clickHandler = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
 
   render(){
     return (
       <View style={styles.container}>
+        <Text>{this.state.counter}</Text>
         <Button title={'click me!'} onPress={this.clickHandler}/>
       </View>
     );
