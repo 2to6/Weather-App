1547b9c60617dd46aa2c21de0bd35abbced80fc2
Choejiwon <jiwon0929@ajou.ac.kr>
2019-07-22 20:36:27 +0900

    Text Color example

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.textBig}>Hello, World!</Text>
+      <Text style={[styles.textBig, styles.textRed]}>Hello, World!</Text>
     </View>
   );
 }
@@ -19,4 +19,7 @@ const styles = StyleSheet.create({
   textBig: {
     fontSize: 32,
   },
+  textRed: {
+    color: 'red'
+  }
 });
