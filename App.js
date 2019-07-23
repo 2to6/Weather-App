906d97a03ba51db1cf7c73b030c41f96a5fb2327
Choejiwon <jiwon0929@ajou.ac.kr>
2019-07-23 14:50:03 +0900

    30. seperate components, 도시리스트 파일을 만들고 보여주는 코드는 CityList에 후, App에서는 바로 CityList를 보여줌

--- App.js
+++ App.js
@@ -1,65 +1,10 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, TouchableOpacity} from 'react-native';
-import { Constants } from 'expo';
+import CityList from './CityList';
 
 export default class App extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      cities: [],
-    };
-  }
-
-  componentDidMount(){
-    fetch('http://demo6468405.mockable.io/weather-crawlers/cities')
-      .then(response => response.json())
-      .then(cities => {
-        console.log('cities = ', cities.length);
-        this.setState({
-          cities
-        });
-      });
-  }
-
-  onPressCity(item){
-    console.log('onPressCity =', item);
-  }
-
-  renderItem(city) {
+  render(){
     return (
-      <TouchableOpacity style={styles.item} onPress={this.onPressCity}>
-        <Text style={styles.text}>{city}</Text>
-      </TouchableOpacity>
-    );
-  }
-  render() {
-    return (
-      <FlatList style={styles.container}
-        renderItem = {({item}) => this.renderItem(item)}
-        keyExtractor={item => item}
-        data = {this.state.cities}
-      />
+      <CityList />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    marginTop: Constants.statusBarHeight,   //핸드폰 상단의 상태표시줄을 건드리지 않게함
-  },
-  item: {
-    flex: 1,
-    height: 50,
-    justifyContent: 'center',
-
-    borderWidth: 1,
-    borderColor: 'orange',
-  },
-  text: {
-    fontSize: 20,
-    textAlign: 'center',
-  }
-});
