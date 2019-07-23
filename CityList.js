a4cc8a3962f52fa53bedeacc3cda8d84a97f5606
Choejiwon <jiwon0929@ajou.ac.kr>
2019-07-23 15:45:17 +0900

    34. Navigate from city list, 이거 보니까 onPress로 화면 넘어가는 법 쉽네

--- CityList.js
+++ CityList.js
@@ -28,6 +28,7 @@ export default class CityList extends React.Component {
 
   onPressCity(item){
       console.log('onPressCity = ', item);
+      this.props.navigation.navigate('Detail');
   }
 
   renderItem(city) {
