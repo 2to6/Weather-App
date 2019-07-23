eb65ddd69f59c8d5b4b953144b34fca258f4def5
Choejiwon <jiwon0929@ajou.ac.kr>
2019-07-23 14:00:24 +0900

    17. border for detecting edges

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Button ,StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
-
-
-export default class App extends React.Component{
-  render(){
+export default class App extends React.Component {
+  render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>1</Text>
-        <Text style={[styles.text, styles.text2]}>2</Text>
-        <Text style={styles.text}>3</Text>
+        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
       </View>
     );
   }
@@ -19,20 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',  
-    justifyContent: 'center',
-  },
-  text:{
-    flex: 1,
-    borderWidth: 1,
     borderColor: 'red',
-
-    textAlign:'center',
-    textAlignVertical: 'center',
-    fontSize: 24,
-    fontWeight: 'bold',
+    borderWidth: 2,
   },
-  text2:{
-    flex: 2,
-  }
 });
